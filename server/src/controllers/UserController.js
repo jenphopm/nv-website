d8cb6f2fd060e72061292720331f7b5300cfc213
jenphopm <jane_auto@hotmail.com>
2019-03-17 00:00:41 +0700

    ep6 mysqlite

--- server/src/controllers/UserController.js
+++ server/src/controllers/UserController.js
@@ -1,26 +1,83 @@
+const { User } = require('../models')
+
 module.exports = {
-    // get all user
-    index (req, res) {
-      res.send('เรียกข้อมูล ผู้ใช้งานทั้งหมด')
-    },
-  
-    // create user
-    create (req, res) {
-      res.send('สร้างผู้ใช้')
-    },
-  
-    // edit user, suspend, active
-    put (req, res) {
-      res.send('แก้ไขผู้ใช้')
-    },
-  
-    // delete user
-    remove (req, res) {
-      res.send('ลบผู้ใช้งาน')
-    },
-  
-    // get user by id
-    show (req, res) {
-      res.send('ดูข้อมูลผู้ใช้งาน')
+  // get all user
+  async index(req, res) {
+    try {
+      const users = await User.findAll()
+      res.send(users)
+    } catch (err) {
+      res.status(500).send({
+        error: 'The users information was incorrect'
+      })
+    }
+  },
+  // create user
+  async create(req, res) {
+    // res.send(JSON.stringify(req.body))
+    try {
+      const user = await User.create(req.body)
+      res.send(user.toJSON())
+    } catch (err) {
+      res.status(500).send({
+        error: 'Create user incorrect'
+      })
+    }
+  },
+  // edit user, suspend, active
+  async put(req, res) {
+    try {
+      await User.update(req.body, {
+        where: {
+          id: req.params.userId
+        }
+      })
+      res.send(req.body)
+    } catch (err) {
+      req.status(500).send({
+        error: 'Update user incorrect'
+      })
+    }
+  },
+  // delete user
+  async remove(req, res) {
+    try {
+      const user = await User.findOne({
+        where: {
+          id: req.params.userId
+        }
+      })
+      if (!user) {
+        return res.status(403).send({
+          error: 'The user information was incorrect'
+        })
+      }
+      await user.destroy()
+      res.send(user)
+    } catch (err) {
+      req.status(500).send({
+        error: 'The user information was incorrect'
+      })
+    }
+  },
+  // get user by id
+  async show(req, res) {
+    try {
+      const user = await User.findOne({
+        where: {
+          id: req.params.userId
+        }
+      }).then(function(user) {
+        if (!user) {
+            return 'not find';
+        }
+        return user.dataValues;
+     })
+      res.send(user)
+    } catch (err) {
+      req.status(500).send({
+        error: 'The user information was incorrect'
+      })
     }
   }
+}
